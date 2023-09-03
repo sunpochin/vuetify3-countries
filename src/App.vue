f8eedafa051ee7ab3db31506fee47c6e4245c794
pac <sunpochin@gmail.com>
2023-09-03 23:44:39 +0800

    give app-bar background color

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-app className="container">
-    <v-app-bar className="container">
-      <h1>Where in the world?</h1>
+  <v-app>
+    <v-app-bar color="grey-lighten-4">
+      <v-app-bar-title>Where in the world?</v-app-bar-title>
     </v-app-bar>
-    <v-main>
+    <v-main class="px-15">
       <router-view />
     </v-main>
   </v-app>
@@ -13,8 +13,4 @@
 //
 </script>
 
-<style>
-.container {
-  margin: 0 6rem;
-}
-</style>
+<style scoped></style>
